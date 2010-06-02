8befaf08d7dd6142618123075334f55be1e5c8e3
Tanner Donovan <ttdonovan@gmail.com>
2010-06-01 23:33:47 -0700

    Added example data: Plate, User, Plate Set to 'db/seeds.rb'. Gives developers some development data to better understand relationships. Only for Rails Env develoment, 'rake db:init' or 'rake db:reset'. TODOs add more examples of Plate, Events and fix Plate Set create plate in 'db/seed.rb'.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+if Rails.env.eql?('development')
+  #
+  # Plates
+  #
+  plate = Plate.create(
+    :layout_name => "plated-layout",
+    :plate_name => "main-content",
+    :description => "A *Plate* is a class of content. A plate is identified by the combination of it's *Layout Name*, *Instance Name* and *Plate Name*. The names are used to classify and organize plates."
+  )
+  
+  # TODO: seeds.rb add examples of Plate and instance names
+  
+  
+  #
+  # Events
+  #
+  # TODO: seeds.rb add examples of various Events
+  
+  
+  #
+  # Users
+  #
+  admin = User.create!(:email => "admin@example.com", :password => "admin", :admin => true)
+  admin.confirm_email!
+  
+  user = User.create!(:email => "user@example.com", :password => "user")
+  user.confirm_email!
+  
+  
+  #
+  # Plate Sets
+  #
+  plate_set = PlateSet.create(
+    :name => "blueprint-plate-set",
+    :layout_name => "blueprint-layout",
+    :description => "A *Plate Set* is a template for generating a group of related *Plates*. For example, if a help page has a body plate and a left column plate, a user can generate an instance of both those plates from that plate set."
+  )
+  PlateSetPlate.create(
+    :plate_name => "blueprint-header",
+    :description => "blueprint's header plate",
+    :plate_set => plate_set
+  )
+  PlateSetPlate.create(
+    :plate_name => "blueprint-footer",
+    :description => "blueprint's footer plate",
+    :plate_set => plate_set
+  )
+  # FIXME: seeds.rb plate_set is not creating plates?
+  # plate_set.create_plates("blueprint", "Created from 'blueprint-plate-set")
+end
